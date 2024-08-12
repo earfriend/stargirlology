25bf2d12d040b784e26495705bf4bfc52fe7ea1a
earfriend <samalsherbert@gmail.com>
2024-08-11 20:12:10 -0400

    Update terms on privacy

--- stargirlology-webapp/pages/legal/privacy.vue
+++ stargirlology-webapp/pages/legal/privacy.vue
@@ -3,5 +3,8 @@
     <h3>Privacy Policy</h3>
     <p>This site uses google firebase and analytics. <a href="https://firebase.google.com/support/privacy/">[1]</a> <a href="https://firebase.google.com/terms/">[2]</a></p>
     <p>Comments will be made public.</p>
+    <p>If you use the contact form or any other means of contacting us please let us know if you do/don't want us to make share your comment.</p>
+    <p>As of this writing we don't validate email addresses, so if you want to be contacted please provide a valid email address.</p>
+    <p>If you don't provide a valid email address we won't be able to reset your password.</p>
   </div>
 </template>
--- stargirlology-webapp/pages/legal/tos.vue
+++ stargirlology-webapp/pages/legal/tos.vue
@@ -4,5 +4,6 @@
     <p>This site uses google firebase and analytics. <a href="https://firebase.google.com/support/privacy/">[1]</a> <a href="https://firebase.google.com/terms/">[2]</a></p>
     <p>No warranty.</p>
     <p>Dont spam.</p>
+    <p>Transcripts are provided as a service and may not be accurate. The site uses Google Text-to-Speech API (Long en-us).</p>
   </div>
 </template>
